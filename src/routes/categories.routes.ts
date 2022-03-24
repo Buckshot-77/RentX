04c31fd4592f2c1e1feddbdf064aea70e471e008
Hugo Oliveira <hugo684@gmail.com>
2022-03-23 23:44:31 -0300

    UUID implementation on category creation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
@@ -6,8 +7,11 @@ const categories: object[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
+    const id = uuidv4();
 
-    categories.push({ name, description });
+    const category = { id, name, description };
+
+    categories.push(category);
 
     console.log(categories);
     return response.status(201).send();
