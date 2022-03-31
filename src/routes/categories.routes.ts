3e2e7c959e7d3c39b8333a45239c53a2d117fe58
Hugo Oliveira <hugo684@gmail.com>
2022-03-31 18:52:03 -0300

    Created CreateCategoryService

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,24 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
-    const { name, description } = request.body;
+    try {
+        const { name, description } = request.body;
 
-    const doesCategoryExist = categoriesRepository.findByName(name);
+        const createCategoryService = new CreateCategoryService(
+            categoriesRepository
+        );
+        createCategoryService.execute({ name, description });
 
-    if (doesCategoryExist) {
-        return response.status(400).json({
-            message: 'A category with the given name already exists.',
-        });
+        return response.status(201).send();
+    } catch (error: any) {
+        return response.status(400).json({ message: error.message });
     }
-    categoriesRepository.create({ name, description });
-
-    return response.status(201).send();
 });
 
 categoriesRoutes.get('/', (request, response) => {
