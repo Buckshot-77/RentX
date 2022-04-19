a7a479e9afa0db693ae856e27f2ce495bc03fae7
Hugo Oliveira <hugo684@gmail.com>
2022-04-18 22:53:36 -0300

    Created index.ts for router

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,11 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
-
 const port = 3333;
 
 app.use(express.json());
-
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
-
-app.get('/', (request, response) => response.json({ message: 'hello' }));
-
-app.post('/courses', (request, response) => {
-    const { name } = request.body;
-
-    return response.json({ name });
-});
+app.use(router);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
