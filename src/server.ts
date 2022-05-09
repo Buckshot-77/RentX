f4c5d913a051c0ab346d89781eaed082f13a040a
Hugo Oliveira <hugo684@gmail.com>
2022-05-08 21:02:27 -0300

    Added docs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 const port = 3333;
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(express.json());
 app.use(router);
